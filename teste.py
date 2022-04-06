365863997581ebb2af2351aa1e7837eca5dd2b2c
EwertonRosendo <ewerton.rosendo15@gmail.com>
2022-04-06 20:36:07 -0300

    update

--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
-from ex107 import moeda
+from ex108 import moeda
 p = float(input("Digite um preço: R$ "))
-print(f"A metade de R${p} é R${moeda.metade(p)}")
-print(f"A metade de R${p} é R${moeda.dobro(p)}")
-print(f"Aumentando 10% de R${p}, temos: R${moeda.aumentar(p)}")
-print(f"Diminuindo 13% de {p}, temos: R${moeda.reduzindo(p)}")
+print(f"A metade de R${p} é {moeda.moeda(moeda.metade(p))}")
+print(f"A metade de R${p} é {moeda.moeda(moeda.dobro(p))}")
+print(f"Aumentando 10% de R${p}, temos: {moeda.moeda(moeda.aumentar(p))}")
+print(f"Diminuindo 13% de R${p}, temos: {moeda.moeda(moeda.reduzindo(p))}")
