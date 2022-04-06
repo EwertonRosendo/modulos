320e96a0da14ab8b194dc96b4f77a006e79f3746
EwertonRosendo <ewerton.rosendo15@gmail.com>
2022-04-06 20:45:27 -0300

    Update

--- ex109/moeda.py
+++ ex109/moeda.py
@@ -1,22 +1,29 @@
-def metade(valor):
+def metade(valor, form=False):
     x = (valor/2)
-    return x
-
-
-def dobro(valor):
+    if form == False:
+        return x
+    else:
+        return "R${:.2f} ".format(x).replace(".", ",")
+def dobro(valor, form=False):
     x = (valor*2)
-    return x
+    if form == False:
+        return x
+    else:
+        return "R${:.2f} ".format(x).replace(".", ",")
 
 
-def aumentar(valor):
+def aumentar(valor, form=False):
     x = ((valor*(10/100)) + valor)
-    return x
+    if form == False:
+        return x
+    else:
+        return "R${:.2f} ".format(x).replace(".", ",")
 
 
-def reduzindo(valor):
+def reduzindo(valor, form=False):
     x = (valor - (valor*(13/100)))
-    return x
+    if form == False:
+        return x
+    else:
+        return "R${:.2f} ".format(x).replace(".", ",")
 
-
-def moeda(valor):
-    return "R${:.2f}".format(valor).replace(".", ",")
--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
-from ex108 import moeda
+from ex109 import moeda
 p = float(input("Digite um preço: R$ "))
-print(f"A metade de R${p} é {moeda.moeda(moeda.metade(p))}")
-print(f"A metade de R${p} é {moeda.moeda(moeda.dobro(p))}")
-print(f"Aumentando 10% de R${p}, temos: {moeda.moeda(moeda.aumentar(p))}")
-print(f"Diminuindo 13% de R${p}, temos: {moeda.moeda(moeda.reduzindo(p))}")
+print(f"A metade de R${p} é {(moeda.metade(p, True))}")
+print(f"A metade de R${p} é {(moeda.dobro(p, True))}")
+print(f"Aumentando 10% de R${p}, temos: {(moeda.aumentar(p, True))}")
+print(f"Diminuindo 13% de R${p}, temos: {(moeda.reduzindo(p, True))}")
